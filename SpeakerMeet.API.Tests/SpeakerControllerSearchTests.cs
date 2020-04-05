fc47985f8e97e85e8650ea48a73abbe1cfe55d6f
va1ery <valery.gourov@gmail.com>
2020-04-05 11:16:24 +0300

    second test succeded

--- SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -6,10 +6,20 @@ namespace SpeakerMeet.API.Tests
 {
     public class SpeakerControllerSearchTests
     {
-        [Fact]
+        [Fact/*(Skip = "No longer needed")*/]
         public void ItExists()
         {
         var controller = new SpeakerController();
         }
+ 
+         [Fact]
+        public void ItHasSearch()
+        {
+        // Arrange
+        var controller = new SpeakerController();
+
+        // Act
+        controller.Search("Jos");
+        }  
     }
 }
--- SpeakerMeet.API/Controllers/SpeakerController.cs
+++ SpeakerMeet.API/Controllers/SpeakerController.cs
@@ -7,9 +7,30 @@ using Microsoft.Extensions.Logging;
 
 namespace SpeakerMeet.API.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class SpeakerController : ControllerBase
+    [Route("api/[controller]")]
+    public class SpeakerController : Controller
     {
+        [HttpGet]
+        [Route("search")]
+        public IActionResult Search(string searchString)
+        {
+            var hardCodedSpeakers = new List<Speaker>
+            {
+                new Speaker {Name = "Josh"},
+                new Speaker {Name = "Joshua"},
+                new Speaker {Name = "Joseph"},
+                new Speaker {Name = "Bill"},
+            };
+
+            var speakers = hardCodedSpeakers
+                .Where(x => x.Name.StartsWith(searchString, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            return Ok(speakers);
+        }
+    }
+        public class Speaker
+    {
+        public string Name { get; set; }
     }
 }
+
