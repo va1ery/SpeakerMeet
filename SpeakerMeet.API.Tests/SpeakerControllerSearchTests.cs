ade88bd384f2d9a7b0ea7d50a81f61c414192a48
va1ery <valery.gourov@gmail.com>
2020-04-05 10:12:22 +0300

    first test succeded

---
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using SpeakerMeet.API.Controllers;
+
+namespace SpeakerMeet.API.Tests
+{
+    public class SpeakerControllerSearchTests
+    {
+        [Fact]
+        public void ItExists()
+        {
+        var controller = new SpeakerController();
+        }
+    }
+}
