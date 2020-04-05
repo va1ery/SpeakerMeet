8a562c430fbf7823c7d3ac96a394de0402552740
val gourov <valery.gourov@gmail.com>
2020-04-05 21:37:34 +0300

    Constructor added

--- SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -7,38 +7,26 @@ using Microsoft.AspNetCore.Mvc;
 namespace SpeakerMeet.API.Tests
 {
     public class SpeakerControllerSearchTests
-    {
-        [Fact(Skip = "No longer needed")]
-        public void ItExists()
-        {
-        var controller = new SpeakerController();
-        }
- 
-         [Fact(Skip = "No longer needed")]
-        public void ItHasSearch()
-        {
-        // Arrange
-        var controller = new SpeakerController();
+    {       
+private readonly SpeakerController _controller;
 
-        // Act
-        controller.Search("Jos");
-        }  
-        [Fact]
-        public void ItReturnsOkObjectResult()
-        {
-        // Arrange
-        var controller = new SpeakerController();
-
-        // Act
-        var result = controller.Search("Jos");
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.IsType<OkObjectResult>(result);
-        }
-         
-    
+public SpeakerControllerSearchTests()
+{
+  _controller = new SpeakerController();
+}       
 [Fact]
+public void GivenExactMatchThenOneSpeakerInCollection()
+{
+  // Arrange
+  // Act
+  var result = _controller.Search("Joshua") as OkObjectResult;
+
+  // Assert
+  var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+  Assert.Equal(1, speakers.Count);
+}
+
+[Fact(Skip = "No longer needed")]
 public void ItReturnsCollectionOfSpeakers()
 {
   // Arrange
